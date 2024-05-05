32b7965f193c372497fbcb01a9963ff636c72bca
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-05-05 04:44:12 -0700

    navbar dark mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,7 +11,7 @@ const Header = (props: {
   setSidebarOpen: (arg0: boolean) => void;
 }) => {
   return (
-    <header className="sticky top-0 z-999 flex w-full bg-graymedium drop-shadow-1 dark:bg-[#21212e] dark:drop-shadow-none">
+    <header className="sticky top-0 z-999 flex w-full bg-graymedium drop-shadow-1 dark:bg-[#30303d] dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           <Link className="block flex-shrink-0 lg:hidden" to="/">
