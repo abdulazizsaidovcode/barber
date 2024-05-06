492af87affa05adc58312e55c0021af917ac0ab9
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-05-06 06:37:21 -0700

    dashboard edit bg

--- src/pages/finance/finance.tsx
+++ src/pages/finance/finance.tsx
@@ -7,7 +7,7 @@ const Finance = () => {
   const tableHeaders = [
     { id: 1, name: 'Ism' },
     { id: 2, name: 'Familiya' },
-    { id: 3, name: 'Yosh' }
+    { id: 3, name: 'Yosh' },
   ];
   return (
     <>
@@ -15,18 +15,19 @@ const Finance = () => {
         <Breadcrumb pageName="Finance" />
         <MainTabs />
         <MasterTable thead={tableHeaders}>
-        <tr>
-          <td className="p-5">Ali</td>
-          <td className="p-5">Valiyev</td>
-          <td className="p-5">25</td>
-        </tr>
-        <tr>
-          <td className="p-5">Bek</td>
-          <td className="p-5">Karimov</td>
-          <td className="p-5">30</td>
-        </tr>
-        // Bu yerda yanada ko'proq qatorlar qo'shishingiz mumkin
-      </MasterTable>      </DefaultLayout>
+          <tr>
+            <td className="p-5">Ali</td>
+            <td className="p-5">Valiyev</td>
+            <td className="p-5">25</td>
+          </tr>
+          <tr>
+            <td className="p-5">Bek</td>
+            <td className="p-5">Karimov</td>
+            <td className="p-5">30</td>
+          </tr>
+          // Bu yerda yanada ko'proq qatorlar qo'shishingiz mumkin
+        </MasterTable>{' '}
+      </DefaultLayout>
     </>
   );
 };
