c698023f3a190ca5882062aa0534a3607117fdc0
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-05-06 17:25:07 +0300

    dashboard cardlar tugadi pashti

--- src/components/CardDataStats.tsx
+++ src/components/CardDataStats.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 
 interface CardDataStatsProps {
   title: string;
@@ -19,12 +19,12 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({
 }) => {
   return (
     <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
-      <div className="mt-4 flex items-end justify-between">
+      <div className="mt-4 flex items-center justify-between ">
         <div>
+          <span className="text-sm font-medium">{title}</span>
           <h4 className="text-title-md font-bold text-black dark:text-white">
             {total}
           </h4>
-          <span className="text-sm font-medium">{title}</span>
         </div>
       </div>
     </div>
--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -9,13 +9,12 @@ import TableOne from '../../components/Tables/TableOne';
 import DefaultLayout from '../../layout/DefaultLayout';
 import { DatePicker, Select } from 'antd';
 import { Option } from 'antd/es/mentions';
-import { DatePickerType } from 'antd/es/date-picker';
 
 const ECommerce: React.FC = () => {
   const [type, setType] = useState<string>('2024');
   return (
     <DefaultLayout>
-      <div className= ' block mb-5 md:flex md:justify-between lg:flex lg:justify-between xl:flex xl:justify-between'>
+      <div className=' block mb-5 md:flex md:justify-between lg:flex lg:justify-between xl:flex xl:justify-between'>
         <h1 className='font-semibold text-black text-3xl dark:text-white'>General indicators</h1>
         <div className='gap-10 md:flex md:gap-10 lg:flex lg:gap-10 xl:flex xl:gap-10'>
           <Select value={type} onChange={setType} className='block w-full h-full md:w-30 lg:w-30 xl:w-30 mb-3  '>
@@ -32,9 +31,25 @@ const ECommerce: React.FC = () => {
       </div>
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-        <CardDataStats title="Total views" total="$3.456K"  >
+        <CardDataStats title="Мастера" total="1 684"  >
         </CardDataStats>
-        <CardDataStats title="Total Profit" total="$45,2K"  >
+        <CardDataStats title="Total Profit" total="3 545"  >
+        </CardDataStats>
+        <CardDataStats title="Заказы" total="10 845"  >
+        </CardDataStats>
+        <CardDataStats title="Отмененные клиент/мастер" total="152 / 253"  >
+        </CardDataStats>
+        <CardDataStats title="Оборот общий" total="100 545 000"  >
+        </CardDataStats>
+        <CardDataStats title="Доход" total="20 785 500"  >
+        </CardDataStats>
+        <CardDataStats title="Отток клиентов" total="1 684"  >
+        </CardDataStats>
+        <CardDataStats title="Отток мастеров" total="10"  >
+        </CardDataStats>
+        <CardDataStats title="Отток клиентов" total="27"  >
+        </CardDataStats>
+        <CardDataStats title="Клиентов на 1 мастера усредненно" total="1 684"  >
         </CardDataStats>
       </div>
 
