ea56bce51cf040834a84f07540e24023356d54c2
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-05-07 15:46:00 +0300

    Dashbiard tugadi

--- src/components/Charts/ChartTwo.tsx
+++ src/components/Charts/ChartTwo.tsx
@@ -93,6 +93,7 @@ const ChartTwo: React.FC = () => {
 
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
+      
       <div>
         <div id="chartTwo" className="-ml-5 -mb-9">
           <ReactApexChart
--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -12,6 +12,7 @@ import ChartFive from '../../components/Charts/ChartFive';
 import ChartSex from '../../components/Charts/ChartSex';
 import ChartSeven from '../../components/Charts/ChartSeven';
 import ChartEight from '../../components/Charts/ChartEight';
+import ChartNine from '../../components/Charts/ChartNine';
 
 const ECommerce: React.FC = () => {
   // const [type, setType] = useState<string>('2024');
@@ -133,6 +134,9 @@ const ECommerce: React.FC = () => {
       </div>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         <ChartEight/>
+      </div>
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <ChartNine/>
       </div>
       
 
