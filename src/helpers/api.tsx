1bb31bba2ea30eb8aeb9c281fc20e493e9ce9ea1
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-05-27 18:20:17 +0500

    api file add

---
+++ src/helpers/api.tsx
@@ -0,0 +1,2 @@
+// swagger url
+export const base_url: string = 'http://45.67.35.86:8080/'
--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -1,35 +1,48 @@
-import {useState} from "react";
-import {BiSolidShow} from "react-icons/bi";
-import {BiSolidHide} from "react-icons/bi";
+import React, { useState } from 'react';
+import { BiSolidShow } from 'react-icons/bi';
+import { BiSolidHide } from 'react-icons/bi';
+import logo from '../../images/logo/logo.jpeg';
+
+interface StylesType {
+  container: string;
+  card: string;
+  title: string;
+  input: string;
+  button: string;
+  hideShowIcon: string;
+}
 
 export const Login = () => {
-  const [username, setUsername] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-  const [show, setShow] = useState<boolean>(true)
-  const styles = {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [show, setShow] = useState<boolean>(true);
+  const styles: StylesType = {
     container: 'min-h-screen flex items-center justify-center bg-gray-100',
     card: 'max-w-md w-full space-y-8 z-10',
-    title: 'mb-6 text-center text-3xl font-semibold text-gray-900',
+    title: 'mb-6 text-center text-3xl font-extrabold text-slate-900',
     input: 'appearance-none rounded-none relative block w-full p-4 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm duration-150',
     button: 'group relative w-full flex justify-center py-2.5 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
     hideShowIcon: 'absolute top-4 right-4 text-[1.5rem] hover:cursor-pointer opacity-60 hover:opacity-90 duration-150 z-40'
   };
 
-  const handleSubmit = (event): void => {
-    event.preventDefault()
-    let data = {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    let data: { username: string, password: string } = {
       username, password
-    }
-    console.log(data.username, data.password)
-  }
+    };
+    console.log(data.username, data.password);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <h2 className={styles.title}>Sign In</h2>
+        <div className={`flex justify-center items-center`}>
+          <img src={logo} alt={`bookers logo`} className={`w-24 h-24 rounded-full`} />
+        </div>
+        <h2 className={styles.title + ' tracking-wide'}>Bookers Beauty</h2>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           <div className="rounded-md shadow-sm -space-y-px">
-            <div className='my-10'>
+            <div className="mb-6">
               <label htmlFor="username" className="sr-only">Username</label>
               <input
                 id="username"
@@ -40,9 +53,9 @@ export const Login = () => {
                 autoComplete="username"
                 required
                 className={styles.input}
-                placeholder="Username"/>
+                placeholder="Username" />
             </div>
-            <div className='my-10 relative'>
+            <div className="relative">
               <label htmlFor="password" className="sr-only">Password</label>
               <input
                 id="password"
@@ -53,20 +66,18 @@ export const Login = () => {
                 autoComplete="current-password"
                 required
                 className={`${styles.input}`}
-                placeholder="Password"/>
+                placeholder="Password" />
               {show
-                ? <BiSolidShow onClick={() => setShow(false)} className={styles.hideShowIcon}/>
-                : <BiSolidHide onClick={() => setShow(true)} className={styles.hideShowIcon}/>
+                ? <BiSolidShow onClick={() => setShow(false)} className={styles.hideShowIcon} />
+                : <BiSolidHide onClick={() => setShow(true)} className={styles.hideShowIcon} />
               }
             </div>
           </div>
-          <div>
-            <button type="submit" className={styles.button}>
-              LOGIN
-            </button>
-          </div>
+          <button type="submit" className={styles.button}>
+            LOGIN
+          </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
