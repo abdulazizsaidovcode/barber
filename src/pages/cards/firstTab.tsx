693729e4dc6c8b47a2c0031b9ef61a7f0bc891e5
omadbekolimov <omadbekolimov96@gmail.com>
2024-05-28 16:06:59 +0500

    ishla

--- src/pages/cards/firstTab.tsx
+++ src/pages/cards/firstTab.tsx
@@ -24,7 +24,7 @@ const FilterComponent: React.FC = () => {
 
   const styles = {
     mainContainer: {
-      padding: '20px',
+      padding: '20px ',
       backgroundColor: '#fff',
       borderRadius: '8px',
       boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
