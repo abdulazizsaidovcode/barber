b19ae7e294ae3d56646e29dcd3a1567edfb6b23c
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-05-28 14:32:34 +0500

    💋

--- src/pages/notification/chat/chat.tsx
+++ src/pages/notification/chat/chat.tsx
@@ -5,9 +5,10 @@ import Notselected from '../components/chat components/notselected';
 import ChatEmptyState from '../components/chat components/emptychat';
 import { Input, Select } from 'antd';
 import { Buttons } from '../../../components/buttons';
-import { IoSearchOutline } from 'react-icons/io5';
+import { IoSearchOutline, IoSend } from 'react-icons/io5';
 import { IoMdAttach } from 'react-icons/io';
 import { FaCheck } from 'react-icons/fa6';
+import SockJS from 'sockjs-client';
 import { Client } from '@stomp/stompjs';
 
 const Chat: React.FC = () => {
