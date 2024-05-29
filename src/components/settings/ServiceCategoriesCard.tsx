8389c2b2eaaa6ea2d8b717466791e944dd789d07
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-05-29 16:01:19 +0500

    settings update

--- src/components/settings/ServiceCategoriesCard.tsx
+++ src/components/settings/ServiceCategoriesCard.tsx
@@ -1,28 +1,65 @@
-import React from "react";
-import { MdEdit, MdDelete  } from "react-icons/md";
+import React, { useState, useRef } from "react";
+import { MdEdit, MdDelete, MdSave } from "react-icons/md";
 
-interface ServiceCategoriesCard {
+interface ServiceCategoriesCardProps {
   title: string;
-  editOnClick: () => void;
+  editOnClick: (newTitle: string) => void;
   deleteOnClick: () => void;
 }
 
-const ServiceCategoriesCard: React.FC<ServiceCategoriesCard> = ({ title, editOnClick, deleteOnClick }) => {
+const ServiceCategoriesCard: React.FC<ServiceCategoriesCardProps> = ({ title, editOnClick, deleteOnClick }) => {
+  const [isEditable, setIsEditable] = useState(false);
+  const [currentTitle, setCurrentTitle] = useState(title);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleEditClick = () => {
+    if (isEditable) {
+      editOnClick(currentTitle);
+      
+    } else {
+      setTimeout(() => {
+        inputRef.current?.focus();
+      }, 0);
+    }
+    setIsEditable(!isEditable);
+  };
+
   return (
     <div className="flex mt-4">
-      <div className="w-full flex items-center p-3 rounded-md border-[1px] dark:border-[#fff] border-[#000]">
-        <p className="dark:text-white">{title}</p>
+      <div>
+        <input
+          ref={inputRef}
+          type="text"
+          value={currentTitle}
+          disabled={!isEditable}
+          onChange={(e) => setCurrentTitle(e.target.value)}
+          placeholder="Type something..."
+          className="dark:bg-[#60606d] w-[323px] border-black h-13 bg-[#f1f5f9] border-[1px] dark:border-white active:outline-none dark:bg-gray-800 dark:text-white rounded-md px-3"
+        />
       </div>
       <div className="flex items-center gap-2 ms-5">
-        <button onClick={editOnClick} className="p-[6px]  dark:border-[#fff] border-[#000] border-[1px] rounded-lg">
-          <MdEdit size={20} color="black" className="dark:text-white"/>
-        </button>
-        <button onClick={deleteOnClick}  className="p-[6px] dark:border-[#fff] border-[1px] border-[#000] rounded-lg">
+        {isEditable ? (
+          <button
+            onClick={() => {
+              handleEditClick();
+              editOnClick(currentTitle);
+              
+            }}
+            className="p-[6px] dark:border-[#fff] border-[#000] border-[1px] rounded-lg"
+          >
+            <MdSave size={20} color="black" className="dark:text-white" />
+          </button>
+        ) : (
+          <button onClick={handleEditClick} className="p-[6px] dark:border-[#fff] border-[#000] border-[1px] rounded-lg">
+            <MdEdit size={20} color="black" className="dark:text-white" />
+          </button>
+        )}
+        <button onClick={deleteOnClick} className="p-[6px] dark:border-[#fff] border-[1px] border-[#000] rounded-lg">
           <MdDelete size={20} color="black" className="dark:text-white" />
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ServiceCategoriesCard;
