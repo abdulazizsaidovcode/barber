822ad2e5cec81de6147ca0e6d0a5d7df1bb661f9
omadbekolimov <omadbekolimov96@gmail.com>
2024-05-29 10:34:18 +0500

    Disabled

--- src/components/MasterCard/masterCard.tsx
+++ src/components/MasterCard/masterCard.tsx
@@ -15,49 +15,48 @@ interface MasterCardProps {
 
 const MasterCard: React.FC<MasterCardProps> = ({ masterName, specialistTitle, phoneNumber, imageUrl, masterData }) => {
     return (
-        <div className='w-full  max-h-screen mb-4 flex flex-wrap'>
-            <div className='p-4 w-full bg-white dark:bg-black  rounded-lg shadow-md mb-4'>
-                <h1 className='font-bold'>История подписки</h1>
+        <div className="w-full max-h-screen mb-4 flex flex-col gap-4">
+            <div className="p-4 w-full bg-white dark:bg-black rounded-lg shadow-md">
+                <h1 className="font-bold">История подписки</h1>
             </div>
-            <div className='bg-white dark:bg-black flex flex-wrap justify-between p-4 w-full  rounded-lg shadow-md mb-4'>
-                <h1 className='font-bold text-xl text-slate-700 dark:text-slate-300'>Premium</h1>
-                <button className='bg-[#58CA64] px-6 rounded-full text-white'>Активный</button>
+            <div className="bg-white dark:bg-black flex justify-between items-center p-4 w-full rounded-lg shadow-md">
+                <h1 className="font-bold text-xl text-slate-700 dark:text-slate-300">Premium</h1>
+                <button className="bg-[#58CA64] px-6 py-2 rounded-full text-white">Активный</button>
             </div>
-            <div className='w-full flex justify-between'>
-                <div className='w-[35%]'>
-                    <div className='w-full bg-white dark:bg-black shadow-lg p-3 rounded-lg mb-3  text-slate-700 dark:text-slate-300'>
-                        <div className='flex justify-between mb-3'>
-                            <h1 className='font-bold'>Мастер</h1>
+            <div className="w-full flex flex-col lg:flex-row gap-4">
+                <div className="w-full lg:w-1/3 flex flex-col gap-4">
+                    <div className="w-full bg-white dark:bg-black shadow-lg p-3 rounded-lg text-slate-700 dark:text-slate-300">
+                        <div className="flex justify-between mb-3">
+                            <h1 className="font-bold">Мастер</h1>
                             <h1>{masterName}</h1>
                         </div>
-                        <div className='flex justify-center mb-3'>
+                        <div className="flex justify-center mb-3">
                             <img
                                 src={imageUrl}
-                                className='rounded-full'
-                                alt='master'
+                                className="rounded-full w-24 h-24 object-cover"
+                                alt="master"
                             />
                         </div>
-                        <h1 className='text-center mb-2'>{specialistTitle}</h1>
-                        <p className='text-center mb-3'>{phoneNumber}</p>
+                        <h1 className="text-center mb-2">{specialistTitle}</h1>
+                        <p className="text-center mb-3">{phoneNumber}</p>
                     </div>
-                    <div className='w-full  text-slate-700 dark:text-slate-300 bg-white dark:bg-black  shadow-lg p-3 rounded-lg mb-3'>
-                        <div className='flex justify-between mb-3 '>
+                    <div className="w-full text-slate-700 dark:text-slate-300 bg-white dark:bg-black shadow-lg p-3 rounded-lg">
+                        <div className="flex justify-between mb-3">
                             <h1>Остановить</h1>
-                            <div className='bg-gray-300 p-1 rounded-full'>
-                                <input type='range' />
+                            <div className="bg-gray-300 p-1 rounded-full">
+                                <input type="range" />
                             </div>
                         </div>
-                        <div className='flex justify-between'>
-                            <h1 className='font-bold mt-2'>Остановить</h1>
-                            <button className='bg-[#9C0A35] px-8 py-2 rounded-lg text-white'>Продлить</button>
+                        <div className="flex justify-between">
+                            <h1 className="font-bold mt-2">Остановить</h1>
+                            <button className="bg-[#9C0A35] px-8 py-2 rounded-lg text-white">Продлить</button>
                         </div>
                     </div>
                 </div>
-
-                <div className='w-[64%] bg-white dark:bg-black  text-slate-700 dark:text-slate-300 shadow-md p-5 mb-3'>
+                <div className="w-full lg:w-2/3 bg-white dark:bg-black text-slate-700 dark:text-slate-300 shadow-md p-5 rounded-lg">
                     {masterData && masterData.map((item, idx) => (
-                        <div className='flex justify-start items-center font-bold mt-8 p-2' key={idx}>
-                            <h1 className='w-48'>{item.name}</h1>
+                        <div className="flex justify-between items-center font-bold mt-4 p-2 border-b border-gray-200 dark:border-gray-700" key={idx}>
+                            <h1 className="w-1/2">{item.name}</h1>
                             <h1>{item.price}</h1>
                         </div>
                     ))}
--- src/components/PricesCard/pricesCard.tsx
+++ src/components/PricesCard/pricesCard.tsx
@@ -1,26 +1,49 @@
-import React from "react";
+import { Button, Checkbox, CheckboxProps, Input } from "antd";
+import React, { useState } from "react";
+import Modal from "../modals/modal";
 
 interface IData {
     name: string;
     btnName: number | string;
 }
+
 const masterData: IData[] = [
     {
-        name : 'Цена',
+        name: 'Цена',
         btnName: 'Активировать'
     },
     {
-        name : 'Оплачено',
+        name: 'Оплачено',
         btnName: 'Активировать'
     },
-]
+];
 
-const PricesCard : React.FC = () => {
-    return(
-        <div className="flex flex-row gap-2">
-            <div className=' w-full bg-white dark:bg-black  text-slate-700 dark:text-slate-300 shadow-6 p-4 mb-3 rounded-lg'>
-                <h1 className='font-bold text-xl  text-slate-700 dark:text-slate-300 mb-2'>Тарифы:</h1>
-                <hr className='text-gray'/>
+const PricesCard: React.FC = () => {
+    const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [sliderValue, setSliderValue] = useState(0);
+
+    const handleCheckboxChange: CheckboxProps["onChange"] = (e) => {
+        setIsCheckboxChecked(e.target.checked);
+    };
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleSliderChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSliderValue(Number(e.target.value));
+    };
+
+    return (
+        <div className="flex flex-col md:flex-row gap-2">
+            <div className='w-full md:w-[35%] bg-white dark:bg-black text-slate-700 dark:text-slate-300 shadow-6 p-4 mb-3 rounded-lg'>
+                <h1 className='font-bold text-xl text-slate-700 dark:text-slate-300 mb-2'>Тарифы:</h1>
+                <hr className='text-gray' />
                 {masterData && masterData.map((item, idx) => (
                     <div className='flex justify-between p-3' key={idx}>
                         <h1 className='font-bold mt-2'>{item.name}</h1>
@@ -28,9 +51,56 @@ const PricesCard : React.FC = () => {
                     </div>
                 ))}
             </div>
-            <div className='w-full  text-slate-700 dark:text-slate-300 bg-white dark:bg-black  shadow-6 p-5 mb-3 rounded-lg'></div>  
+            <div className='w-full md:w-[64%] text-slate-700 dark:text-slate-300 bg-white dark:bg-black shadow-6 p-5 mb-3 rounded-lg'>
+                <div className="flex flex-row justify-between mb-3">
+                    <h1 className="font-bold">Остановка подписки:</h1>
+                    <div className="text-end">
+                        <Checkbox onChange={handleCheckboxChange}>Без возврата</Checkbox>
+                    </div>
+                </div>
+                <hr />
+                <p className="mb-2">Сумма возрата</p>
+                <div className="flex flex-col md:flex-row justify-between mb-1 text-slate-700 dark:text-slate-300">
+                    <Input placeholder="3 000 000" className="py-2 text-dark font-bold mr-48" disabled={isCheckboxChecked} />
+                    <Checkbox disabled={isCheckboxChecked}>Карта</Checkbox>
+                </div>
+                <div className="flex flex-col md:flex-row justify-between mb-2 text-slate-700 dark:text-slate-300">
+                    <Checkbox disabled={isCheckboxChecked}>Вся сумма подписки</Checkbox>
+                    <Checkbox disabled={isCheckboxChecked}>Вся сумма подписки</Checkbox>
+                </div>
+                <p className="mb-1">Дата с которой остановить тариф</p>
+                <div className="flex flex-col md:flex-row font-bold text-black justify-between mb-2">
+                    <Input type="date" className="px-8 py-1 rounded-lg shadow-6 mb-2 mr-24" disabled={isCheckboxChecked} />
+                    <button className="bg-[#A6A6A6] rounded-lg px-2 mb-2" disabled={isCheckboxChecked}>Отменить</button>
+                    <button
+                        className={`rounded-lg px-2 ${isCheckboxChecked ? "bg-red-600" : "bg-[#A6A6A6]"} mb-2`}
+                        onClick={handleOpenModal}
+                        disabled={!isCheckboxChecked}
+                    >
+                        Остановить
+                    </button>
+                </div>
+            </div>
+            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+                <p>Вы уверены что хоите остановить подписку?</p>
+                <div className="flex justify-end mt-4">
+                    <button 
+                        onClick={handleCloseModal} 
+                        className="mr-2 bg-[#232323] px-2 py-1 text-white"
+                        disabled={sliderValue < 100}
+                    >
+                        Остановить
+                    </button>
+                    <button 
+                        onClick={handleCloseModal} 
+                        className="bg-[#686868] px-4 py-1 text-white"
+                    >
+                        Нет
+                    </button>
+                </div>
+            </Modal>
         </div>
-              
     );
 };
-export default PricesCard
+
+export default PricesCard;
