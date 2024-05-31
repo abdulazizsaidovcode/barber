841a247bebbf51fec1b4592b2221163668fc5341
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-05-31 15:49:49 +0500

    🎉

--- src/pages/notification/mails/addMails.tsx
+++ src/pages/notification/mails/addMails.tsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import { Buttons } from '../../../components/buttons'
 
 function AddMails() {
-  return (
-    <div>
-      <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search branch name..." required />
-    </div>
-  )
+    return (
+        <div>
+            <p> Название темы:</p>
+            <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Новый тариф" required />
+            <p>Описание:</p>
+            <div>
+                <textarea name="" id=""></textarea>
+
+            </div>
+            <div>
+                <p>Вложения</p>
+                <p>Прикрепить картинку</p>
+            </div>
+            <input type="file"/>
+            <p>Вложить файл</p>
+            <div className='flex gap-3'>
+                <div className='flex gap-2'>
+                    <input type="checkbox" name="" id="" />
+                    <p>Всем</p>
+                </div>
+                <div className='flex gap-3'>
+                    <input type="checkbox" name="" id="" />
+                    <p>Мастерам</p>
+                </div>
+                <div className='flex gap-2'>
+                    <input type="checkbox" name="" id="" />
+                    <p>Клиентам</p>
+                </div>
+            </div>
+            <div>
+                <button>Назвад</button>
+                <Buttons>Отправить</Buttons>
+            </div>
+        </div>
+    )
 }
 
 export default AddMails
--- src/pages/notification/mails/index.tsx
+++ src/pages/notification/mails/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Product } from '../../../types/product';
 import ProductOne from '../../../images/product/product-01.png';
 import ProductTwo from '../../../images/product/product-02.png';
@@ -5,8 +6,8 @@ import ProductThree from '../../../images/product/product-03.png';
 import ProductFour from '../../../images/product/product-04.png';
 import { DatePicker, Select } from 'antd';
 import { Buttons } from '../../../components/buttons';
-import { Link } from 'react-router-dom';
 import AddMails from './addMails';
+import { TbArrowBigLeftFilled } from 'react-icons/tb';
 
 const productData: Product[] = [
     {
@@ -44,88 +45,108 @@ const productData: Product[] = [
 ];
 
 const ChatTable = () => {
+    const [showAddMails, setShowAddMails] = useState(false);
+
     return (
         <section>
-            <div className='mb-5 flex gap-2'>
-                <Select
-                    className='w-full md:w-40 lg:w-40 xl:w-40 dark:bg-gray-800 dark:text-white'
-                    defaultValue="2024"
-                    style={{ width: 120 }}
-                    dropdownStyle={{ backgroundColor: '#4b4b64', color: 'white' }}
-                    options={[
-                        { value: '2024', label: '2024' },
-                        { value: '2025', label: '2025' },
-                        { value: '2026', label: '2026' },
-                    ]}
-                />
-                <DatePicker
-                    className='h-8 w-full md:w-50 lg:w-50 xl:w-50 dark:bg-gray-800 dark:text-white '
-                    placeholder='Дата'
-                />
-            </div>
-            <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-                <div className="py-6 px-4 md:px-6 xl:px-7.5">
-                    <h4 className="text-xl font-semibold text-black dark:text-white">
-                        Top Products
-                    </h4>
-                </div>
+            {showAddMails ? (
+                <div>
+                    <div onClick={() => setShowAddMails(false)} className='mb-5 flex'>
 
-                <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
-                    <div className="col-span-3 flex items-center">
-                        <p className="font-medium">Картинка</p>
+                        <Buttons className="mb-5">
+                            <TbArrowBigLeftFilled />
+                            
+                        </Buttons>
                     </div>
-                    <div className="col-span-2 hidden items-center sm:flex">
-                        <p className="font-medium">Тема</p>
-                    </div>
-                    <div className="col-span-1 flex items-center">
-                        <p className="font-medium">Кому</p>
-                    </div>
-                    <div className="col-span-1 flex items-center">
-                        <p className="font-medium">Дата</p>
-                    </div>
-                    <div className="col-span-1 flex items-center">
-                        <p className="font-medium">Вложения</p>
-                    </div>
-                    <div className="col-span-1 flex items-center">
-                        <p className="font-medium">Описание</p>
-                    </div>
+                    <AddMails />
                 </div>
+            ) : (
+                <div>
+                    <div className='mb-5 flex gap-2'>
+                        <Select
+                            className='w-full md:w-40 lg:w-40 xl:w-40 dark:bg-gray-800 dark:text-white'
+                            defaultValue="2024"
+                            style={{ width: 120 }}
+                            dropdownStyle={{ backgroundColor: '#4b4b64', color: 'white' }}
+                            options={[
+                                { value: '2024', label: '2024' },
+                                { value: '2025', label: '2025' },
+                                { value: '2026', label: '2026' },
+                            ]}
+                        />
+                        <DatePicker
+                            className='h-8 w-full md:w-50 lg:w-50 xl:w-50 dark:bg-gray-800 dark:text-white '
+                            placeholder='Дата'
+                        />
+                        <div onClick={() => setShowAddMails(true)}>
+                            <Buttons >
+                                Создать рассылку
+                            </Buttons>
+                        </div>
 
-                {productData.map((product, key) => (
-                    <div
-                        className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
-                        key={key}
-                    >
-                        <div className="col-span-3 flex items-center">
-                            <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-                                <div className="h-12.5 w-15 rounded-md">
-                                    <img src={product.image} alt="Product" />
-                                </div>
-                                <p className="text-sm text-black dark:text-white">
-                                    {product.name}
-                                </p>
+                    </div>
+                    <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                        <div className="py-6 px-4 md:px-6 xl:px-7.5">
+                            <h4 className="text-xl font-semibold text-black dark:text-white">
+                                Top Products
+                            </h4>
+                        </div>
+                        <div className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5">
+                            <div className="col-span-3 flex items-center">
+                                <p className="font-medium">Картинка</p>
+                            </div>
+                            <div className="col-span-2 hidden items-center sm:flex">
+                                <p className="font-medium">Тема</p>
+                            </div>
+                            <div className="col-span-1 flex items-center">
+                                <p className="font-medium">Кому</p>
+                            </div>
+                            <div className="col-span-1 flex items-center">
+                                <p className="font-medium">Дата</p>
+                            </div>
+                            <div className="col-span-1 flex items-center">
+                                <p className="font-medium">Вложения</p>
+                            </div>
+                            <div className="col-span-1 flex items-center">
+                                <p className="font-medium">Описание</p>
                             </div>
                         </div>
-                        <div className="col-span-2 hidden items-center sm:flex">
-                            <p className="text-sm text-black dark:text-white">
-                                {product.category}
-                            </p>
-                        </div>
-                        <div className="col-span-1 flex items-center">
-                            <p className="text-sm text-black dark:text-white">
-                                ${product.price}
-                            </p>
-                        </div>
-                        <div className="col-span-1 flex items-center">
-                            <p className="text-sm text-black dark:text-white">{product.sold}</p>
-                        </div>
-                        <div className="col-span-1 flex items-center">
-                            <p className="text-sm text-meta-3">${product.profit}</p>
-                        </div>
+                        {productData.map((product, key) => (
+                            <div
+                                className="grid grid-cols-6 border-t border-stroke py-4.5 px-4 dark:border-strokedark sm:grid-cols-8 md:px-6 2xl:px-7.5"
+                                key={key}
+                            >
+                                <div className="col-span-3 flex items-center">
+                                    <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
+                                        <div className="h-12.5 w-15 rounded-md">
+                                            <img src={product.image} alt="Product" />
+                                        </div>
+                                        <p className="text-sm text-black dark:text-white">
+                                            {product.name}
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="col-span-2 hidden items-center sm:flex">
+                                    <p className="text-sm text-black dark:text-white">
+                                        {product.category}
+                                    </p>
+                                </div>
+                                <div className="col-span-1 flex items-center">
+                                    <p className="text-sm text-black dark:text-white">
+                                        ${product.price}
+                                    </p>
+                                </div>
+                                <div className="col-span-1 flex items-center">
+                                    <p className="text-sm text-black dark:text-white">{product.sold}</p>
+                                </div>
+                                <div className="col-span-1 flex items-center">
+                                    <p className="text-sm text-meta-3">${product.profit}</p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <AddMails/>
+                </div>
+            )}
         </section>
     );
 };
