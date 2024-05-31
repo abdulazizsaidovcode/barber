338ad5e6982f2872c568223477c36865a33fd324
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-05-31 18:46:32 +0500

    we

--- src/pages/settings/Specializations.tsx
+++ src/pages/settings/Specializations.tsx
@@ -7,14 +7,14 @@ import Modal from '../../components/modals/modal';
 
 const Specializations: React.FC = () => {
   const [isInputOpen, setIsInputOpen] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleInput = () => {
-    setIsInputOpen(!isInputOpen);
+    setIsInputOpen((prev) => !prev);
   };
 
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <>
@@ -26,31 +26,31 @@ const Specializations: React.FC = () => {
           <Accordion title='Красота волос'>
             <div className='flex justify-between'>
               <div className='w-[75%]'>
-                <ServiceCategoriesCard title='Красота волос' editOnClick={() => 'w'} deleteOnClick={() => openModal()} />
+                <ServiceCategoriesCard title='Красота волос' editOnClick={() => {}} deleteOnClick={openModal} />
               </div>
               <div className='mt-5'>
                 <button
                   className='bg-[#eaeaea] text-black dark:text-white dark:bg-[#9C0A35] rounded-lg py-2 px-10'
                   onClick={toggleInput}
+                  aria-label='Add new input'
                 >
                   <FaPlus size={25} />
                 </button>
-
               </div>
             </div>
-            <div className='mt-5'>
-              {isInputOpen && (
+            {isInputOpen && (
+              <div className='mt-5'>
                 <input
                   type='text'
                   placeholder='Type something...'
-                  className='dark:bg-[#60606d]  w-[66.6%] border-black h-13 bg-[#f1f5f9] border-[1px] dark:border-white active:outline-none dark:bg-gray-800 dark:text-white rounded-md px-3 '
+                  className='dark:bg-[#60606d] w-[66.6%] border-black h-13 bg-[#f1f5f9] border-[1px] dark:border-white active:outline-none dark:bg-gray-800 dark:text-white rounded-md px-3'
                 />
-              )}
-            </div>
+              </div>
+            )}
           </Accordion>
         </div>
       </DefaultLayout>
-      <Modal isOpen={isOpen} onClose={closeModal}>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
         <div className="w-[500px] h-[130px]">
           <div className="flex justify-center">
             <p className="text-xl text-black">Вы уверены что хоите удалить процедуру?</p>
