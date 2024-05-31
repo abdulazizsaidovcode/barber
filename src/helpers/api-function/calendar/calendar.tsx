56faf398622c8d6b58256c933249d316608ef129
Behruz1227 <behruzxonniyozov12@gmail.com>
2024-05-31 18:44:33 +0500

    REV

--- src/helpers/api-function/calendar/calendar.tsx
+++ src/helpers/api-function/calendar/calendar.tsx
@@ -23,17 +23,13 @@ export const getCategoryId = (setCategory: (data: []) => void) => {
 };
 
 
-export const getCalendar = (setCategory: (data: []) => void) => {
-    axios
-      .get(service_category_list, config)
-      .then((res) => {
-        if (res.data.success) {
-          setCategory(res.data.body);
-        } else {
-          console.log("null");
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+// export const getCalendar = () => {
+//     axios
+//       .get(`${calendar_url}?localDate=2024-05-30`, config)
+//       .then((res) => {
+//         console.log(res);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//       });
+//   };
