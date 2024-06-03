7dde22146cb330df54635dabe73a9f0aec800b1b
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-06-03 13:06:46 +0500

    requests update

---
+++ src/components/request/pages/requestSpecializations.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RequestLayout from '../../../pages/request/request'
+
+const RequestSpecializations: React.FC = () => {
+  return (
+        <RequestLayout>
+            <div>
+                dwqd
+            </div>
+        </RequestLayout>
+  )
+}
+
+export default RequestSpecializations
--- src/pages/request/request.tsx
+++ src/pages/request/request.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import DefaultLayout from '../../layout/DefaultLayout'
-import RequestSidebar from '../../components/request/sidebar/RequestSidebar'
+import { ReactNode } from 'react';
+import DefaultLayout from '../../layout/DefaultLayout';
+import RequestSidebar from '../../components/request/sidebar/RequestSidebar';
 
-const Request: React.FC = () => {
+const RequestLayout = ({ children } : { children: ReactNode }) => {
     return (
         <DefaultLayout padding={true}>
             <div className='w-full flex justify-between'>
                 <p className='w-1/4 p-2'>
-                    <RequestSidebar/>
+                    <RequestSidebar />
                 </p>
-                <div className='w-3/4'>fer</div>
+                <div className='w-3/4'>{children}</div>
             </div>
         </DefaultLayout>
-    )
-}
+    );
+};
 
-export default Request
+export default RequestLayout;
