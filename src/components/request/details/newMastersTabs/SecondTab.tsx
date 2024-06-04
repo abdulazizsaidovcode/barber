2d41d3273ec1ba700bfc31dab57e93ebc1c8deea
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-06-04 13:57:26 +0500

    new masteras upadate

--- src/components/request/details/newMastersTabs/SecondTab.tsx
+++ src/components/request/details/newMastersTabs/SecondTab.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FirstTabCard from './cards/FirstTabCard'
+import Modal from '../../../modals/modal'
+import img from '../../../../images/Image.png';
 
-const SecondTab = () => {
+const SecondTab: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true)
+  const closeModal = () => setIsOpen(false)
+
   return (
     <div>
-      <FirstTabCard/>
+      <FirstTabCard
+        category='Стрижка, укладка, милирование:'
+        description='В услугу входит мытьё головы, массаж головы и Разнообразный и богатый опыт основанный .......'
+        price='350 000 сум' duration='1 час 30 минут'
+        openModal={openModal}
+      />
+      <Modal isOpen={isOpen} onClose={closeModal}>
+        <div className="flex justify-center items-center h-full w-full">
+          <img className="max-w-full max-h-full rounded-md" src={img} alt="Modal Image" />
+        </div>
+      </Modal>
     </div>
   )
 }
--- src/components/request/details/newMastersTabs/cards/FirstTabCard.tsx
+++ src/components/request/details/newMastersTabs/cards/FirstTabCard.tsx
@@ -1,32 +1,40 @@
-import React from 'react'
-import img from '../../../../../images/Image.png'
+import React from 'react';
+import img from '../../../../../images/Image.png';
 
-const FirstTabCard: React.FC = () => {
+interface FirstTabCardProp {
+    category: string;
+    price: string;
+    duration: string;
+    description: string;
+    openModal: () => void
+}
+
+const FirstTabCard: React.FC<FirstTabCardProp> = ({ category, price, duration, description, openModal }) => {
     return (
         <div className='bg-[#cccccc] dark:bg-white px-4 flex w-full h-60 py-3'>
-            <div className='w-[22%] mx-1 flex items-center'>
-                <img src={img} alt="" />
+            <div className='w-[22%] mx-1 flex items-center' onClick={openModal}>
+                <img src={img} alt={category} />
             </div>
             <div className='w-[70%] mx-1'>
                 <div>
-                    <p className='text-2xl'>Стрижка, укладка, милирование:</p>
+                    <p className='text-2xl'>{category}</p>
                 </div>
                 <div className='my-3'><hr /></div>
                 <div className='flex justify-between w-[100%]'>
                     <div className='flex flex-col gap-7'>
                         <p className='text-xl font-semibold'>Цена</p>
                         <p className='text-xl font-semibold'>Длительность</p>
-                        <p className='text-xl font-semibold'>Instagram</p>
+                        <p className='text-xl font-semibold'>Описание</p>
                     </div>
-                    <div className='flex flex-col gap-7 ms-34'>
-                        <p className='text-xl'>350 000 сум</p>
-                        <p className='text-xl'>1 час 30 минут</p>
-                        <p className='text-xl'>В услугу входит мытьё головы, массаж головы и Разнообразный и богатый опыт основанный .......</p>
+                    <div className='flex flex-col gap-7'>
+                        <p className='text-xl'>{price}</p>
+                        <p className='text-xl'>{duration}</p>
+                        <p className='text-xl'>{description}</p>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default FirstTabCard
+export default FirstTabCard;
