bbf70cf77234fc9ee6910ab1310098f995caa97d
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-06-11 17:17:38 +0300

    sdsdsd

--- src/pages/Client/Client.tsx
+++ src/pages/Client/Client.tsx
@@ -5,15 +5,17 @@ import { items } from './data.tsx';
 import { useEffect } from 'react';
 import { getRegion } from '../../helpers/api-function/master/master.tsx';
 import clientFilterStore from '../../helpers/state_managment/client/clientFilterStore.tsx';
+import { useTranslation } from 'react-i18next';
 
 const Client = () => {
-  const {setRegionData} = clientFilterStore()
+  const { t } = useTranslation();
+  const { setRegionData } = clientFilterStore()
   useEffect(() => {
     getRegion(setRegionData)
   }, [])
   return (
     <DefaultLayout>
-      <Breadcrumb pageName="Client" />
+      <Breadcrumb pageName={t("Client")} />
       <Tabs
         className="dark:bg-boxdark bg-white p-2 w-full"
         defaultActiveKey="1"
