97f510bf0fe3eb00e5ace55d9fd4694c90158ee0
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-06-11 19:01:02 +0500

    faqat file upload qoldi

--- src/pages/documents/masterDocument.tsx
+++ src/pages/documents/masterDocument.tsx
@@ -4,20 +4,20 @@ import Switch from '../../components/settings/details/TableSwitcher';
 import { MdEdit } from 'react-icons/md';
 import FileUploader from '../../components/FileDowlander';
 import helpStore from '../../helpers/state_managment/help/helpStore.tsx';
-import { updateIsActive } from '../../helpers/api-function/help/help.tsx';
+import { updateHelp, updateIsActive } from '../../helpers/api-function/help/help.tsx';
 import masterStore from '../../helpers/state_managment/master/masterStore.tsx';
 import { Toaster } from 'react-hot-toast';
-
+import { Buttons } from '../../components/buttons';
+import Modal from '../../components/modals/modal.tsx';
 
 const MasterDocument: React.FC = () => {
-  const { dataMaster, setDataMaster, setUpdateTextArea } = helpStore();
-  const { isModal, setIsModal } = masterStore();
+  const { dataMaster, setDataMaster, setUpdateTextArea, updateTextArea } = helpStore();
+  const { isModal, setIsModal, isLoading, setIsLoading } = masterStore();
   const [modalVal, setModalVal] = useState<{ text: string; active: boolean }>({ text: '', active: false });
 
   const trueFalse = (status: string) => status !== 'SERVICE_SPECIFICATION';
   const openModal = () => setIsModal(!isModal);
 
-  console.log(modalVal);
   return (
     <div className="p-2">
       <Toaster position={`top-center`} />
@@ -60,6 +60,41 @@ const MasterDocument: React.FC = () => {
       <button className={`bg-[#9C0A35] text-white px-3 py-2 rounded-lg`}>
         Сохранить изменения
       </button>
+
+      <Modal isOpen={isModal} onClose={openModal}>
+        <div className={`w-[12rem] sm:w-[18rem] md:w-[25rem] lg:w-[30rem]`}>
+          <div className={`flex flex-col justify-center`}>
+            <p className={`font-bold text-xl text-black dark:text-white opacity-80 text-center`}>
+              Help message update
+            </p>
+          </div>
+          <div className={`mt-5`}>
+            <textarea
+              placeholder={`update message...`}
+              className={`w-full px-3 py-2 outline-0 border border-graydark dark:border-white dark:bg-black dark:text-white rounded-lg`}
+              rows={5}
+              value={modalVal.text}
+              onChange={e => setModalVal({ ...modalVal, text: e.target.value })}
+            ></textarea>
+            <div className={`flex justify-start items-center gap-5 mt-5`}>
+              <input
+                className={`w-5 h-5 rounded-full`}
+                type="checkbox"
+                id={`helpActiveInputMaster`}
+                checked={modalVal.active}
+                onChange={e => setModalVal({ ...modalVal, active: e.target.checked })}
+              />
+              <label htmlFor={`helpActiveInputMaster`} className={`text-lg text-black dark:text-white font-semibold`}>Active</label>
+            </div>
+            <div className={`flex justify-center items-center gap-6 mt-5`}>
+              <Buttons bWidth={`w-[150px]`} onClick={() => updateHelp(updateTextArea, setDataMaster, 'FOR_MASTER', modalVal, setIsLoading, openModal)}>
+                {isLoading ? 'loading...' : 'save'}
+              </Buttons>
+              <Buttons bWidth={`w-[150px]`} onClick={openModal}>close</Buttons>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
