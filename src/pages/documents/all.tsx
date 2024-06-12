51198db7b2ae5db4b0de7cdd9375dbc550ed5379
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-06-12 15:38:42 +0500

    document full complated

--- src/pages/documents/all.tsx
+++ src/pages/documents/all.tsx
@@ -5,7 +5,7 @@ import FileUploader from '../../components/FileDowlander';
 import { MdEdit } from 'react-icons/md';
 import helpStore from '../../helpers/state_managment/help/helpStore.tsx';
 import { HelpList } from '../../types/help.ts';
-import { updateHelp, updateIsActive, updateSaveButtons } from '../../helpers/api-function/help/help.tsx';
+import { deleteHelpFile, updateHelp, updateIsActive, updateSaveButtons } from '../../helpers/api-function/help/help.tsx';
 import Modal from '../../components/modals/modal.tsx';
 import masterStore from '../../helpers/state_managment/master/masterStore.tsx';
 import { Buttons } from '../../components/buttons';
@@ -13,13 +13,15 @@ import { useTranslation } from 'react-i18next';
 import FileGetUploader from '../../components/FileDowlanderGet.tsx';
 
 const All: React.FC = () => {
-  const { dataAll, setDataAll, updateTextArea, setUpdateTextArea, filesList, uploadFileID, setSelectedFilesDef } = helpStore();
+  const { dataAll, setDataAll, updateTextArea, setUpdateTextArea, filesList, uploadFileID, setSelectedFilesDef, deleteFileId } = helpStore();
   const { isModal, setIsModal, isLoading, setIsLoading } = masterStore();
   const [modalVal, setModalVal] = useState<{ text: string; active: boolean }>({ text: '', active: false });
+  const [deleteFile, setDeleteFile] = useState(false)
   const { t } = useTranslation();
 
   const trueFalse = (status: string) => status !== 'LICENSES' && status !== 'CERTIFICATES';
   const openIsModal = () => setIsModal(!isModal);
+  const openDeleteModal = () => setDeleteFile(!deleteFile);
 
   function accordionList(params: boolean, id: number | string, name: string | null, item: HelpList) {
     return (
@@ -31,7 +33,7 @@ const All: React.FC = () => {
           </div>
         )}
         <div className={`mt-3 ${params && 'flex justify-between items-center'} text-slate-700 dark:text-slate-300`}>
-          {!params && <FileGetUploader getList={item.attachments} openModal={() => console.log('')} idIn={item.id} />}
+          {!params && <FileGetUploader getList={item.attachments} openModal={openDeleteModal} idIn={item.id} />}
           <div className="flex gap-3 items-center my-3">
             <p>Отображать в приложениях</p>
             <Switch
@@ -63,32 +65,34 @@ const All: React.FC = () => {
         {dataAll.map(item => (
           <Accordion
             title={
-              item.helpStatus === 'ABOUT_SERVICE' ? t("About_the_service")
-                : item.helpStatus === 'PRIVACY_POLICY' ? t("Privacy_Policy")
-                  : item.helpStatus === 'LICENSE_AGREEMENT' ? t("License_agreement")
-                    : item.helpStatus === 'LICENSES' ? t("License")
-                      : item.helpStatus === 'CERTIFICATES' ? t("Certificates_and_other_documents")
-                        : t("Using_the_application")
+              item.helpStatus === 'ABOUT_SERVICE' ? t('About_the_service')
+                : item.helpStatus === 'PRIVACY_POLICY' ? t('Privacy_Policy')
+                  : item.helpStatus === 'LICENSE_AGREEMENT' ? t('License_agreement')
+                    : item.helpStatus === 'LICENSES' ? t('License')
+                      : item.helpStatus === 'CERTIFICATES' ? t('Certificates_and_other_documents')
+                        : t('Using_the_application')
             }
             key={item.id}>
             {accordionList(trueFalse(item.helpStatus), item.id, item.text, item)}
           </Accordion>
         ))}
       </div>
-      <button className={`bg-[#9C0A35] text-white px-3 py-2 rounded-lg mt-4`} onClick={() => updateSaveButtons(filesList, uploadFileID, setDataAll, 'ALL', setSelectedFilesDef)}>
-        {t("Save_changes")}
+      <button className={`bg-[#9C0A35] text-white px-3 py-2 rounded-lg mt-4`}
+              onClick={() => updateSaveButtons(filesList, uploadFileID, setDataAll, 'ALL', setSelectedFilesDef)}>
+        {t('Save_changes')}
       </button>
 
+      {/*update modal*/}
       <Modal isOpen={isModal} onClose={openIsModal}>
         <div className={`w-[12rem] sm:w-[18rem] md:w-[25rem] lg:w-[30rem]`}>
           <div className={`flex flex-col justify-center`}>
             <p className={`font-bold text-xl text-black dark:text-white opacity-80 text-center`}>
-              {t("Help_message_update")}
+              {t('Help_message_update')}
             </p>
           </div>
           <div className={`mt-5`}>
             <textarea
-              placeholder={t("update_message")}
+              placeholder={t('update_message')}
               className={`w-full px-3 py-2 outline-0 border border-graydark dark:border-white dark:bg-black dark:text-white rounded-lg`}
               rows={5}
               value={modalVal.text}
@@ -102,14 +106,38 @@ const All: React.FC = () => {
                 checked={modalVal.active}
                 onChange={e => setModalVal({ ...modalVal, active: e.target.checked })}
               />
-              <label htmlFor={`helpActiveInput`} className={`text-lg text-black dark:text-white font-semibold`}>{t("Active")}</label>
+              <label htmlFor={`helpActiveInput`}
+                     className={`text-lg text-black dark:text-white font-semibold`}>{t('Active')}</label>
             </div>
             <div className={`flex justify-center items-center gap-6 mt-5`}>
-              <Buttons bWidth={`w-[150px]`} onClick={() => updateHelp(updateTextArea, setDataAll, 'ALL', modalVal, setIsLoading, openIsModal)}>
-                {isLoading ? t("Loading") : t("Save")}
+              <Buttons bWidth={`w-[150px]`}
+                       onClick={() => updateHelp(updateTextArea, setDataAll, 'ALL', modalVal, setIsLoading, openIsModal)}>
+                {isLoading ? t('Loading') : t('Save')}
               </Buttons>
-              <Buttons bWidth={`w-[150px]`} onClick={openIsModal}>{t("Close")}</Buttons>
+              <Buttons bWidth={`w-[150px]`} onClick={openIsModal}>{t('Close')}</Buttons>
             </div>
+          </div>
+        </div>
+      </Modal>
+
+      {/*  file delete modal*/}
+      <Modal isOpen={deleteFile} onClose={openDeleteModal}>
+        <div className={`w-[12rem] sm:w-[18rem] md:w-[25rem] lg:w-[30rem]`}>
+          <div className={`flex flex-col justify-center`}>
+            <p className={`font-bold text-xl text-black dark:text-white opacity-80 text-center`}>
+              Bu fileni uchirishga ishonchingiz komilmi?
+            </p>
+          </div>
+          <div className={`mt-5`}>
+          </div>
+          <div className={`flex justify-center items-center gap-6 mt-5`}>
+            <Buttons
+              bWidth={`w-[150px]`}
+              onClick={() => deleteHelpFile(deleteFileId, setIsLoading, setDataAll, 'ALL', openDeleteModal)}
+            >
+              {isLoading ? 'loading...' : 'O\'chirish'}
+            </Buttons>
+            <Buttons bWidth={`w-[150px]`} onClick={openDeleteModal}>{t('Close')}</Buttons>
           </div>
         </div>
       </Modal>
--- src/pages/documents/masterDocument.tsx
+++ src/pages/documents/masterDocument.tsx
@@ -4,7 +4,7 @@ import Switch from '../../components/settings/details/TableSwitcher';
 import { MdEdit } from 'react-icons/md';
 import FileUploader from '../../components/FileDowlander';
 import helpStore from '../../helpers/state_managment/help/helpStore.tsx';
-import { updateHelp, updateIsActive, updateSaveButtons } from '../../helpers/api-function/help/help.tsx';
+import { deleteHelpFile, updateHelp, updateIsActive, updateSaveButtons } from '../../helpers/api-function/help/help.tsx';
 import masterStore from '../../helpers/state_managment/master/masterStore.tsx';
 import { Buttons } from '../../components/buttons';
 import Modal from '../../components/modals/modal.tsx';
@@ -19,13 +19,16 @@ const MasterDocument: React.FC = () => {
     updateTextArea,
     setSelectedFilesDef,
     uploadFileID,
-    filesList
+    filesList,
+    deleteFileId
   } = helpStore();
   const { isModal, setIsModal, isLoading, setIsLoading } = masterStore();
   const [modalVal, setModalVal] = useState<{ text: string; active: boolean }>({ text: '', active: false });
+  const [deleteFile, setDeleteFile] = useState(false);
 
   const trueFalse = (status: string) => status !== 'SERVICE_SPECIFICATION';
   const openModal = () => setIsModal(!isModal);
+  const openDeleteModal = () => setDeleteFile(!deleteFile);
   const { t } = useTranslation();
 
   return (
@@ -43,7 +46,7 @@ const MasterDocument: React.FC = () => {
               <div
                 className={`mt-3 ${trueFalse(item.helpStatus) && 'flex justify-between items-center'} text-slate-700 dark:text-slate-300`}>
                 {!trueFalse(item.helpStatus) &&
-                  <FileGetUploader getList={item.attachments} openModal={openModal} idIn={item.id} />}
+                  <FileGetUploader getList={item.attachments} openModal={openDeleteModal} idIn={item.id} />}
                 <div className="flex gap-3 items-center my-4">
                   <p>{t('Show_in_apps')}</p>
                   <Switch
@@ -76,6 +79,7 @@ const MasterDocument: React.FC = () => {
         {t('Save_changes')}
       </button>
 
+      {/*update modal*/}
       <Modal isOpen={isModal} onClose={openModal}>
         <div className={`w-[12rem] sm:w-[18rem] md:w-[25rem] lg:w-[30rem]`}>
           <div className={`flex flex-col justify-center`}>
@@ -111,6 +115,26 @@ const MasterDocument: React.FC = () => {
               </Buttons>
               <Buttons bWidth={`w-[150px]`} onClick={openModal}>{t('Close')}</Buttons>
             </div>
+          </div>
+        </div>
+      </Modal>
+
+      {/*  file delete modal*/}
+      <Modal isOpen={deleteFile} onClose={openDeleteModal}>
+        <div className={`w-[12rem] sm:w-[18rem] md:w-[25rem] lg:w-[30rem]`}>
+          <div className={`flex flex-col justify-center`}>
+            <p className={`font-bold text-xl text-black dark:text-white opacity-80 text-center`}>
+              Bu fileni uchirishga ishonchingiz komilmi?
+            </p>
+          </div>
+          <div className={`mt-5`}>
+          </div>
+          <div className={`flex justify-center items-center gap-6 mt-5`}>
+            <Buttons bWidth={`w-[150px]`}
+                     onClick={() => deleteHelpFile(deleteFileId, setIsLoading, setDataMaster, 'FOR_MASTER', openDeleteModal)}>
+              {isLoading ? 'loading...' : 'O\'chirish'}
+            </Buttons>
+            <Buttons bWidth={`w-[150px]`} onClick={openDeleteModal}>{t('Close')}</Buttons>
           </div>
         </div>
       </Modal>
