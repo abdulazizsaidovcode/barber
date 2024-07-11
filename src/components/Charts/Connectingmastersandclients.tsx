d5d32a7e01759b0d31bade9cb2f7a97e52e8f9f8
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-07-11 10:44:33 +0300

    endi hammasi tug'ri bulyapti

--- src/components/Charts/Connectingmastersandclients.tsx
+++ src/components/Charts/Connectingmastersandclients.tsx
@@ -7,83 +7,85 @@ import { DatePicker, Skeleton } from 'antd';
 import { config } from '../../helpers/token';
 import { useTranslation } from 'react-i18next';
 
-const options: ApexOptions = {
-  colors: ['#000000', '#D9D9D9'],
-  chart: {
-    fontFamily: 'Satoshi, sans-serif',
-    type: 'bar',
-    height: 335,
-    stacked: true,
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-  },
-
-  responsive: [
-    {
-      breakpoint: 1536,
-      options: {
-        plotOptions: {
-          bar: {
-            borderRadius: 0,
-            columnWidth: '59%',
-          },
-        },
-      },
-    },
-  ],
-  plotOptions: {
-    bar: {
-      horizontal: false,
-      borderRadius: 0,
-      columnWidth: '25%',
-      borderRadiusApplication: 'end',
-      borderRadiusWhenStacked: 'last',
-
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-
-  xaxis: {
-    categories: ['January', 'February', 'March', 'April', 'May', 'Juny', 'July', 'August', 'September', 'October', 'November', 'December'],
-  },
-  legend: {
-    position: 'top',
-    horizontalAlign: 'left',
-    fontFamily: 'Satoshi',
-    fontWeight: 500,
-    fontSize: '14px',
-
-    markers: {
-      radius: 99,
-    },
-  },
-  fill: {
-    opacity: 1,
-  },
-};
-
-interface ChartTwoState {
-  series: {
-    name: string;
-    data: number[];
-  }[];
-}
-
 const ChartTwo: React.FC = () => {
   const { t } = useTranslation();
   const [clientData, setClientData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [dates, setDates] = useState<string | number>(new Date().getFullYear());
-  const [series, setSeries] = useState<ChartTwoState['series']>([
+  const [series, setSeries] = useState([
     { name: 'MasterCount', data: [] },
     { name: 'Client Count', data: [] },
   ]);
+
+  const options: ApexOptions = {
+    colors: ['#000000', '#D9D9D9'],
+    chart: {
+      fontFamily: 'Satoshi, sans-serif',
+      type: 'bar',
+      height: 335,
+      stacked: true,
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+    },
+    responsive: [
+      {
+        breakpoint: 1536,
+        options: {
+          plotOptions: {
+            bar: {
+              borderRadius: 0,
+              columnWidth: '59%',
+            },
+          },
+        },
+      },
+    ],
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        borderRadius: 0,
+        columnWidth: '25%',
+        borderRadiusApplication: 'end',
+        borderRadiusWhenStacked: 'last',
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    xaxis: {
+      categories: [
+        t('january'),
+        t('february'),
+        t('March'),
+        t('April'),
+        t('May'),
+        t('June'),
+        t('July'),
+        t('August'),
+        t('September'),
+        t('October'),
+        t('November'),
+        t('December')
+      ],
+    },
+    legend: {
+      position: 'top',
+      horizontalAlign: 'left',
+      fontFamily: 'Satoshi',
+      fontWeight: 500,
+      fontSize: '14px',
+      markers: {
+        radius: 99,
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+  };
 
   useEffect(() => {
     getData(dates);
@@ -125,7 +127,7 @@ const ChartTwo: React.FC = () => {
         <h1 className='font-semibold w-75 text-black text-2xl dark:text-white'>{t("Dynamics_of_connecting_masters_and_clients")}</h1>
         <DatePicker onChange={handleYearChange} picker="year" placeholder={t("Select_year")} style={{ height: 35 }} />
       </div>
-      <div className=" rounded-3xl border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark ">
+      <div className="rounded-3xl border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
         <div id="chartTwo" className="-ml-5 -mb-9 mx-auto">
           {loading ? (
             <Skeleton active paragraph={{ rows: 10 }} />
