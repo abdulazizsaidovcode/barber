360f4d1a6aed79e42c207e679d54455f92ea6658
Omadbek <omadbekolimov96@gmail.com>
2024-12-02 15:43:14 +0500

    buldi

--- src/pages/Mutual_settlements/firstTab.tsx
+++ src/pages/Mutual_settlements/firstTab.tsx
@@ -1,15 +1,47 @@
 // FilterComponent.tsx
 import React, { useState } from 'react';
-import { Select, Input, Button, Row, Col, Popover } from 'antd';
+import { Select, Input, Button, Row, Col, Popover, Space, DatePicker } from 'antd';
 import { DownOutlined, UpOutlined } from '@ant-design/icons';
 import { IoSearchOutline } from 'react-icons/io5';
 import MasterTable from '../../components/Tables/MasterTable';
 import { Link, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import masterStore from '../../helpers/state_managment/master/masterStore';
+import { Buttons } from '../../components/buttons';
+import { downloadExcelFile } from '../../helpers/attachment/file-download';
+import { master_download } from '../../helpers/api';
 
 const { Option } = Select;
+const { RangePicker } = DatePicker;
 
 const FilterComponent: React.FC = () => {
+  const {
+    setData,
+    setTotalPage,
+    setDistrictData,
+    setFilters,
+    regionData,
+    districtData,
+    filters,
+    filterObj,
+    category,
+    isLoading,
+    setIsLoading,
+    page,
+    size
+  } = masterStore();
+  const queryParams: string = [
+    filters.searchValue ? `fullName=${filters.searchValue}` : '',
+    filters.regionValue ? `regionId=${filters.regionValue}` : '',
+    filters.cityValue ? `districtId=${filters.cityValue}` : '',
+    // datePicker(0) ? `startDate=${datePicker(0)}` : '',
+    // datePicker(1) ? `endDate=${datePicker(1)}` : '',
+    filters.serviceCategoryValue ? `categoryId=${filters.serviceCategoryValue}` : '',
+    filters.statusValue ? `statusName=${filters.statusValue}` : '',
+    filters.selfEmployedStatusValue === true ? `selfEmployed=true` : filters.selfEmployedStatusValue === false ? `selfEmployed=false` : '',
+    filters.placeOfWorkValue ? `workPlace=${filters.placeOfWorkValue}` : ''
+  ].filter(Boolean).join('&');
+  const url: string = `${master_download}?${queryParams}${queryParams ? '&' : ''}page=${page}&size=${size}`;
   const [showExtraFilters, setShowExtraFilters] = useState(false);
   const [activeMenu, setActiveMenu] = useState<number | null>(null);
   const [open, setOpen] = useState(false);
@@ -23,7 +55,8 @@ const FilterComponent: React.FC = () => {
 
 
   const toggleExtraFilters = () => setShowExtraFilters(!showExtraFilters);
-
+  const resetFilters = (): void => setFilters(filterObj);
+  const handleInputChange = (key: string, value: any) => setFilters({ ...filters, [key]: value });
   const toggleContextMenu = (id: number) => {
     setActiveMenu(activeMenu === id ? null : id);
   };
@@ -112,78 +145,97 @@ const FilterComponent: React.FC = () => {
 
   return (
     <div style={styles.mainContainer} className="dark:bg-boxdark">
-      {/* Top filters row */}
-      <Row gutter={[16, 16]} style={{ marginBottom: '10px' }}>
-        <Col xs={24} sm={12} md={6} style={styles.filterGroup}>
-          <Input
-            placeholder={t("Search_by_name")}
-            prefix={<IoSearchOutline />}
-            style={styles.filterInput}
-          />
-        </Col>
-        <Col xs={24} sm={12} md={6} style={styles.filterGroup}>
-          <Select defaultValue="Country" style={styles.filterInput}>
-            <Option value="toshkent">Toshkent</Option>
-            <Option value="qarshi">Qarshi</Option>
-          </Select>
-        </Col>
-        <Col xs={24} sm={12} md={6} style={styles.filterGroup}>
-          <Select
-            defaultValue="2024"
-            style={styles.filterInput}
-            placeholder={t("Select_year")}
-          >
-            <Option value="2024">2024</Option>
-            <Option value="2023">2023</Option>
-          </Select>
-        </Col>
-        <Col
-          xs={24}
-          sm={12}
-          md={6}
-          style={styles.filterGroup}
-          className="flex gap-4"
-        >
-          <Button
-            className="flex items-center justify-center dark:bg-black"
-            onClick={toggleExtraFilters}
-            style={styles.toggleButton}
-          >
-            {showExtraFilters ? <UpOutlined /> : <DownOutlined />}
-          </Button>
-          <Button style={styles.extraButton}>{t("Download")}</Button>
-        </Col>
-      </Row>
-
-      {/* Extra filters row */}
-      {showExtraFilters && (
-        <Row gutter={[16, 16]} style={{ marginBottom: '10px' }}>
-          <Col xs={24} sm={12} md={6} style={styles.filterGroup}>
-            <Input placeholder={t("Status")} style={styles.filterInput} />
+        <Row gutter={[16, 16]} style={{ marginTop: '1rem' }}>
+          <Col xs={24} sm={12} md={6} className={`mb-[16px]`}>
+            <Input
+              placeholder={t('Search_by_name')}
+              value={filters.searchValue}
+              prefix={<IoSearchOutline />}
+              allowClear
+              className={`w-full bg-white rounded-[8px]`}
+              onChange={(e) => handleInputChange('searchValue', e.target.value)}
+            />
           </Col>
-          <Col xs={24} sm={12} md={6} style={styles.filterGroup}>
-            <Select defaultValue={t("Service_category")} style={styles.filterInput}>
-              <Option value="toshkent">Toshkent</Option>
-              <Option value="qarshi">Qarshi</Option>
-            </Select>
-          </Col>
-          <Col xs={24} sm={12} md={6} style={styles.filterGroup}>
+          <Col xs={24} sm={12} md={6} className={`mb-[16px]`}>
             <Select
-              defaultValue="2024"
-              style={styles.filterInput}
-              placeholder={t("Tarif")}
+              placeholder={t('Region')}
+              value={filters.regionValue}
+              className={`w-full bg-white rounded-[8px]`}
+              allowClear
+              onChange={(value) => handleInputChange('regionValue', value)}
             >
-              <Option value="2024">2024</Option>
-              <Option value="2023">2023</Option>
+              {regionData.length > 0 && regionData.map(item => (
+                <Option value={item.id} key={item.id}>{item.name}</Option>
+              ))}
             </Select>
           </Col>
-          <Col xs={24} sm={12} md={6} style={styles.filterGroup}>
-            <Button style={styles.extraButton}>{t("Reset")}</Button>
+          <Col xs={24} sm={12} md={6} className={`mb-[16px]`}>
+            <Select
+              placeholder={t('City')}
+              value={filters.cityValue}
+              className={`w-full bg-white rounded-[8px]`}
+              allowClear
+              onChange={(value) => handleInputChange('cityValue', value)}
+            >
+              {districtData.length > 0 && districtData.map(item => (
+                <Option value={item.id} key={item.id}>{item.name}</Option>
+              ))}
+            </Select>
+          </Col>
+          <Col xs={24} sm={12} md={6} className="flex gap-4 mb-[16px]">
+            <Buttons onClick={toggleExtraFilters}>
+              {showExtraFilters ? <UpOutlined /> : <DownOutlined />}
+            </Buttons>
+            <Buttons
+              onClick={() => downloadExcelFile(url, setIsLoading, t('File_downloaded_successfully'), t('There_was_an_error_fetching_the_data'), page)}>
+              {isLoading ? t('Loading') : t('Download')}
+            </Buttons>
           </Col>
         </Row>
-      )}
-      {/* Table */}
-      <div>
+        {showExtraFilters && (
+          <>
+            <Row gutter={[16, 16]} style={{ marginTop: '10px' }}>
+              <Col xs={24} sm={12} md={6} className={`mb-[16px] w-full`}>
+                <Space direction="vertical" size={14}>
+                  <RangePicker
+                    placeholder={[t('Select_start_date'), t('Select_end_date')]}
+                    value={filters.registrationPeriodValue}
+                    className={`w-full`}
+                    onChange={(date) => handleInputChange('registrationPeriodValue', date)}
+                  />
+                </Space>
+              </Col>
+              <Col xs={24} sm={12} md={6} className={`mb-[16px]`}>
+                <Select
+                  placeholder={t('Service_category')}
+                  value={filters.serviceCategoryValue}
+                  className={`w-full bg-white rounded-[8px]`}
+                  allowClear
+                  onChange={(value) => handleInputChange('serviceCategoryValue', value)}
+                >
+                  {category.length > 0 && category.map(item => (
+                    <Option value={item.id} key={item.id}>{item.name}</Option>
+                  ))}
+                </Select>
+              </Col>
+              <Col xs={24} sm={12} md={6} className={`mb-[16px]`}>
+                <Select
+                  placeholder={t('Status')}
+                  value={filters.statusValue}
+                  className={`w-full bg-white rounded-[8px]`}
+                  allowClear
+                  onChange={(value) => handleInputChange('statusValue', value)}
+                >
+                  <Option value="ACTIVE">{t('Active')}</Option>
+                  <Option value="BLOCKED">{t('Locked')}</Option>
+                </Select>
+              </Col>
+              <Col xs={24} sm={12} md={6} className={`mb-[16px]`}>
+                <Buttons onClick={resetFilters}>{t('Reset')}</Buttons>
+              </Col>
+            </Row>
+          </>
+        )}
         <MasterTable thead={tableHeaders}>
           {tableData.map((data, index) => (
             <tr key={index} className="dark:text-white text-center">
@@ -223,7 +275,6 @@ const FilterComponent: React.FC = () => {
           ))}
         </MasterTable>
       </div>
-    </div>
   );
 };
 
