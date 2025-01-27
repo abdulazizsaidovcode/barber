79bca2d28f011e7340e212f29f66130e35f04a18
Abdulvohid31 <abdulvohidcode@gmail.com>
2025-01-27 11:21:40 +0500

    sa

--- src/pages/documents/WebTab.tsx
+++ src/pages/documents/WebTab.tsx
@@ -14,34 +14,31 @@ import { config } from '../../helpers/token.tsx';
 
 
 const WebTab = () => {
-    const [dataWeb, setDataWeb] =  useState<any>()
+    const [dataWeb, setDataWeb] = useState<any>()
     const [isModal, setIsModal] = useState(false);
     const getApi = async () => {
         const res = await axios.get('http://207.154.246.120:8080/api/help/type?HELP_TYPE=ALL', config);
         setDataWeb(res?.data?.body);
-        // console.log('data web',res.data.body);
     }
     useEffect(() => {
         getApi();
     }, []);
 
     const openModal = () => setIsModal(!isModal);
-    
+
     return (
         <div className="p-2">
             <div className="flex flex-col gap-3 mb-3 ">
                 {dataWeb && dataWeb?.map((item: any) => (
                     <Accordion title={t('Terms_of_use')}>
-                        {item.text && (
-                            <div
-                                className="border-[1px] text-[.8rem] sm:text-base px-2 sm:px-5 py-3 rounded-xl dark:border-white bg-white dark:bg-black text-slate-700 dark:text-slate-300">
-                                {item.text}
-                            </div>
-                        )}
+                        <div
+                            className="border-[1px] text-[.8rem] sm:text-base px-2 sm:px-5 py-3 rounded-xl dark:border-white bg-white dark:bg-black text-slate-700 dark:text-slate-300">
+                            {item.text}
+                        </div>
                         <div className={`${item.text ? 'mt-3' : ''} flex justify-end items-center text-slate-700 dark:text-slate-300`}>
                             <div className="flex gap-3 items-center my-7 text-slate-700 dark:text-slate-300">
-                             <p className={`text-[.8rem] sm:text-base`}>{t('Show_in_apps')}</p>
-                             {/* <Switch
+                                <p className={`text-[.8rem] sm:text-base`}>{t('Show_in_apps')}</p>
+                                {/* <Switch
                                isOn={item.active}
                                handleToggle={() => updateIsActive(item, setDataClient, 'FOR_CLIENT')}
                              /> */}
@@ -82,8 +79,8 @@ const WebTab = () => {
                             placeholder={`update message...`}
                             className={`w-full px-3 py-2 outline-0 border border-graydark dark:border-white dark:bg-black dark:text-white rounded-lg`}
                             rows={5}
-                            // value={modalVal.text}
-                            // onChange={e => setModalVal({ ...modalVal, text: e.target.value })}
+                        // value={modalVal.text}
+                        // onChange={e => setModalVal({ ...modalVal, text: e.target.value })}
                         ></textarea>
                         {/*<div className={`flex justify-start items-center gap-5 mt-5`}>*/}
                         {/*  <input*/}
@@ -100,7 +97,7 @@ const WebTab = () => {
                             <Buttons bWidth={`w-[150px]`} onClick={openModal}>{t('Close')}</Buttons>
                             <Buttons
                                 bWidth={`w-[150px]`}
-                                // onClick={() => updateHelp(updateTextArea, setDataClient, 'FOR_CLIENT', modalVal, setIsLoading, openModal)}
+                            // onClick={() => updateHelp(updateTextArea, setDataClient, 'FOR_CLIENT', modalVal, setIsLoading, openModal)}
                             >
                                 {/* {isLoading ? t('Loading') : t('Save')} */}e
                             </Buttons>
